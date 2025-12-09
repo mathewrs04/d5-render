ec1d6a47b08c906d4e26ce95a51976470ef30a36
Mathew Reynald <164150651+mathewrs04@users.noreply.github.com>
2025-12-09 21:33:21 +0700

    Add files via upload

---
+++ app-files/data.js
@@ -0,0 +1,41 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-whatsapp-image-2025-12-09-at-211913",
+      "name": "WhatsApp Image 2025-12-09 at 21.19.13",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "yaw": -0.18632165847671978,
+        "pitch": 0.40044168670642044,
+        "fov": 1.488074497006701
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -0.14727731198365213,
+          "pitch": 0,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "Kamar Mandi Tiffany",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
